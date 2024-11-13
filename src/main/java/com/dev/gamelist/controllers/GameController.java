3c682f0a5ae2eb80cbc904a62e4075fec342c294
thaleswillreis <thaleswill@yahoo.com.br>
2024-11-13 20:40:30 -0300

    feat:Implementação da busca por ID

--- src/main/java/com/dev/gamelist/controllers/GameController.java
+++ src/main/java/com/dev/gamelist/controllers/GameController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.dev.gamelist.dto.GameDTO;
 import com.dev.gamelist.dto.GameMinDTO;
 import com.dev.gamelist.services.GameService;
 
@@ -17,9 +19,13 @@ public class GameController {
 	@Autowired
 	private GameService gameService;
 
+	@GetMapping(value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		return gameService.findById(id);
+	}
+	
 	@GetMapping
 	public List<GameMinDTO> findAll() {
-		List<GameMinDTO> result = gameService.findAll();
-		return result;
+		return gameService.findAll();
 	}
 }
--- src/main/java/com/dev/gamelist/services/GameService.java
+++ src/main/java/com/dev/gamelist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.dev.gamelist.dto.GameDTO;
 import com.dev.gamelist.dto.GameMinDTO;
 import com.dev.gamelist.entities.Game;
 import com.dev.gamelist.repositories.GameRepository;
@@ -14,7 +16,14 @@ public class GameService {
 
 	@Autowired
 	private GameRepository gameRepository;
-
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);
+	}
+	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll() {
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
